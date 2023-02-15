a5f83b3dfda54b66e1cf82a51b5005c0cec24a85
CWhite2127 <cwhitewesterly2@gmail.com>
2023-02-14 21:21:43 -0700

    pseudocode added to help with coding the JS for project

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+// Declare Variables
+// for this projectwe will need to track 
+    // our timer
+    // if the quiz taker gets a question right or wrong
+    // total score
+    // we will need
+    // a start button
+    // an array of objects populated with questions
+    // a means of navigating through our questions (see carosel in unit activity?)
+    // a means of stopping the game when the timer hits 0 or all questions are answered
+    // a means of penalizing incorrect answers with time dduction 
+    // a prompt to save initials with scores
+    // use of local storage 
+
+//function
+    // when the start button is pressed
+    // timer starts
+    // the first question is provided
+    
+    // when an answer is selected
+    // right
+        // adds to score 
+        // provides correct feedback
+        // moves to next question
+    // wrong
+        // penalizes timer
+        // provides incorrect feedback
+        // moves to next question
+
+    // timer hits 0 
+    // game over screen is provided
+    // provides screen to save initials
+
+    // all questions are answered
+    // game over screen is provided
+    // provides screen to save initials
+
